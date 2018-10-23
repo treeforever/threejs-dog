3fbde320f11a2a23e84def98e4d5f93c64c1f379
Mengqi Zhu <treeforever7@gmail.com>
2018-10-22 23:33:47 -0400

    add body

--- src/index.js
+++ src/index.js
@@ -140,6 +140,11 @@ function Lion() {
     shading: THREE.FlatShading,
   });
 
+  this.lightPinkMat = new THREE.MeshLambertMaterial({
+    color: 0xffd177,
+    shading: THREE.FlatShading,
+  });
+
   this.whiteMat = new THREE.MeshLambertMaterial({
     color: 0xffffff,
     shading: THREE.FlatShading,
@@ -160,40 +165,56 @@ function Lion() {
     shading: THREE.FlatShading,
   });
 
-  var head1 = new THREE.BoxGeometry(200, 350, 100);
-  var ear1 = new THREE.BoxGeometry(100, 50, 30);
-  var ear2 = new THREE.BoxGeometry(100, 50, 30);
-  var eye1 = new THREE.BoxGeometry(60, 35, 30);
-  var eye2 = new THREE.BoxGeometry(60, 35, 30);
+  // head
+  var head1 = new THREE.BoxGeometry(160, 100, 300);
+  var head2 = new THREE.BoxGeometry(160, 80, 150);
+  var ear1 = new THREE.BoxGeometry(100, 160, 15);
+  var ear2 = new THREE.BoxGeometry(100, 160, 15);
+  var eye1 = new THREE.BoxGeometry(25, 25, 30);
+  var eye2 = new THREE.BoxGeometry(25, 25, 30);
+  var nose = new THREE.BoxGeometry(35, 35, 30);
 
   this.head1 = new THREE.Mesh(head1, this.yellowMat);
+  this.head2 = new THREE.Mesh(head2, this.yellowMat);
   this.ear1 = new THREE.Mesh(ear1, this.pinkMat);
   this.ear2 = new THREE.Mesh(ear2, this.pinkMat);
   this.eye1 = new THREE.Mesh(eye1, this.pinkMat);
   this.eye2 = new THREE.Mesh(eye2, this.pinkMat);
+  this.nose = new THREE.Mesh(nose, this.pinkMat);
 
-  this.ear1.position.x = 150;
-  this.ear1.position.z = 0;
-  this.ear1.position.y = 125;
+  this.head2.position.set(0, 90, -75);
 
-  this.ear2.position.x = -150;
-  this.ear2.position.z = 0;
-  this.ear2.position.y = 125;
-
-  this.eye1.position.x = 80;
-  this.eye1.position.z = 65;
-  this.eye1.position.y = 30;
-
-  this.eye2.position.x = -80;
-  this.eye2.position.z = 65;
-  this.eye2.position.y = 30;
+  this.ear1.position.set(130, 85, -135);
+  this.ear2.position.set(-130, 85, -135);
+  this.eye1.position.set(40, 90, 0);
+  this.eye2.position.set(-40, 90, 0);
+  this.nose.position.set(0, 50, 165);
 
   this.threegroup.add(this.head1);
+  this.threegroup.add(this.head2);
   this.threegroup.add(this.ear1);
   this.threegroup.add(this.ear2);
   this.threegroup.add(this.eye1);
   this.threegroup.add(this.eye2);
+  this.threegroup.add(this.nose);
 
+  // body
+  var upperBody = new THREE.BoxGeometry(160, 300, 100);
+  this.upperBody = new THREE.Mesh(upperBody, this.yellowMat);
+  this.upperBody.position.set(0, -200, -100);
+  this.threegroup.add(this.upperBody);
+
+  var middleBody = new THREE.BoxGeometry(160, 200, 300);
+  this.middleBody = new THREE.Mesh(middleBody, this.yellowMat);
+  this.middleBody.position.set(0, -250, -300);
+  this.threegroup.add(this.middleBody);
+
+  var bottom = new THREE.BoxGeometry(260, 250, 200);
+  this.bottom = new THREE.Mesh(bottom, this.yellowMat);
+  this.bottom.position.set(0, -225, -550);
+  this.threegroup.add(this.bottom);
+
+  // don't care now
   this.threegroup.traverse(function(object) {
     if (object instanceof THREE.Mesh) {
       object.castShadow = true;
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
+    hot: true,
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: '3D dog',
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   output: {
     filename: '[name].bundle.js',
