601187e6094b8a38ec684decd2f4659f7cee69f6
treeforever <treeforever7@gmail.com>
2018-12-02 23:44:38 -0500

    add babel and test framework

---
+++ src/tests/index.unit.js
@@ -0,0 +1,9 @@
+import test from "ava";
+import rule3 from "../index";
+
+test("rule3", t => {
+  var xTarget = 0;
+  var windowHalfX = 400;
+  // var result = rule3(xTarget, 37.5, 62.5, 0, windowHalfX / 2);
+  t.is(0, 400);
+});
