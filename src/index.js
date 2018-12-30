1ec47616a7d3b8fb97de596f53ebd151dbc8c2bf
treeforever <treeforever7@gmail.com>
2018-12-30 11:11:19 -0500

    streamline index.js

--- src/index.js
+++ src/index.js
@@ -19,102 +19,40 @@ import "@babel/polyfill";
 import OrbitControls from "./OrbitControls";
 import { Dog } from "./dog";
 
-//THREEJS RELATED VARIABLES
-
-var scene,
-  camera,
-  controls,
-  fieldOfView,
-  aspectRatio,
-  nearPlane,
-  farPlane,
-  shadowLight,
-  backLight,
-  light,
-  renderer,
-  container;
-
-//SCENE
-var floor,
-  fan,
-  isBlowing = false;
-
-//SCREEN VARIABLES
-
-var HEIGHT,
-  WIDTH,
-  windowHalfX,
-  windowHalfY,
-  mousePos = {
-    x: 0,
-    y: 0
-  };
-
-var onMouseDownPosition;
-
-//INIT THREE JS, SCREEN AND MOUSE EVENTS
-
-function init() {
-  scene = new Scene();
-  HEIGHT = window.innerHeight;
-  WIDTH = window.innerWidth;
-  aspectRatio = WIDTH / HEIGHT;
-  fieldOfView = 60;
-  nearPlane = 1;
-  farPlane = 2000;
-  camera = new PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
-
-  camera.position.x = 700;
-  camera.position.z = 800;
-  camera.position.y = 400;
-  camera.lookAt(new Vector3(0, 0, 0));
-
-  renderer = new WebGLRenderer({
-    alpha: true,
-    antialias: true
-  });
-  renderer.setSize(WIDTH, HEIGHT);
-  renderer.shadowMap.enabled = true;
-  container = document.getElementById("world");
-  container.appendChild(renderer.domElement);
-  windowHalfX = WIDTH / 2;
-  windowHalfY = HEIGHT / 2;
-  controls = new OrbitControls(camera, renderer.domElement);
-
-  controls.update();
-
-  document.addEventListener("mousemove", handleMouseMove, false);
+function createWorldContainer() {
+  const el = document.createElement("div");
+  el.setAttribute("id", "world");
+  return el;
 }
 
+let mousePos = {
+  x: 0,
+  y: 0
+};
 function handleMouseMove(event) {
   mousePos = { x: event.clientX, y: event.clientY };
 }
 
-function addGrid() {
-  var gridHelper = new GridHelper(1000, 50);
-  scene.add(gridHelper);
-}
-
 function createLights() {
-  light = new HemisphereLight(0xffffff, 0xffffff, 0.5);
+  const light = new HemisphereLight(0xffffff, 0xffffff, 0.5);
 
-  shadowLight = new DirectionalLight(0xffffff, 0.8);
+  const shadowLight = new DirectionalLight(0xffffff, 0.8);
   shadowLight.position.set(200, 200, 200);
   shadowLight.castShadow = true;
   // shadowLight.shadowDarkness = 0.2; // shadowDarkness has been removed
 
-  backLight = new DirectionalLight(0xffffff, 0.4);
+  const backLight = new DirectionalLight(0xffffff, 0.4);
   backLight.position.set(-100, 200, 50);
   // backLight.shadowDarkness = 0.1; // shadowDarkness has been removed
   backLight.castShadow = true;
 
-  scene.add(backLight);
   scene.add(light);
   scene.add(shadowLight);
+  scene.add(backLight);
 }
 
 function createFloor() {
-  floor = new Mesh(
+  const floor = new Mesh(
     new PlaneBufferGeometry(2000, 2000),
     new MeshBasicMaterial({
       color: 0xf9d3c5
@@ -127,8 +65,33 @@ function createFloor() {
   scene.add(floor);
 }
 
+function addGrid() {
+  const gridHelper = new GridHelper(1000, 50);
+  scene.add(gridHelper);
+}
+
 function addDogToScene(dog) {
   scene.add(dog);
+}
+
+function render() {
+  if (controls) {
+    controls.update();
+  }
+  renderer.render(scene, camera);
+}
+
+function loop(dog) {
+  function renderLoop(timestamp) {
+    const xTarget = mousePos.x - windowHalfX;
+    const yTarget = mousePos.y - windowHalfY;
+    render();
+
+    dog.moveWithCursor(xTarget, yTarget);
+
+    requestAnimationFrame(renderLoop);
+  }
+  renderLoop();
 }
 
 function sleep(millsecs) {
@@ -150,41 +113,67 @@ async function tongueController(dog) {
   }
 }
 
-function loop(dog) {
-  function renderLoop(timestamp) {
-    var xTarget = mousePos.x - windowHalfX;
-    var yTarget = mousePos.y - windowHalfY;
-    render();
+//INIT THREE JS, SCREEN AND MOUSE EVENTS
+let scene,
+  HEIGHT,
+  WIDTH,
+  aspectRatio,
+  fieldOfView,
+  nearPlane,
+  farPlane,
+  camera,
+  renderer,
+  controls,
+  windowHalfX,
+  windowHalfY;
 
-    dog.moveWithCursor(xTarget, yTarget);
+function init() {
+  scene = new Scene();
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+  aspectRatio = WIDTH / HEIGHT;
+  fieldOfView = 60;
+  nearPlane = 1;
+  farPlane = 2000;
 
-    requestAnimationFrame(renderLoop);
-  }
-  renderLoop();
+  camera = new PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
+  camera.position.x = 700;
+  camera.position.z = 800;
+  camera.position.y = 400;
+  camera.lookAt(new Vector3(0, 0, 0));
+
+  renderer = new WebGLRenderer({
+    alpha: true,
+    antialias: true
+  });
+  renderer.setSize(WIDTH, HEIGHT);
+  renderer.shadowMap.enabled = true;
+  render();
+
+  document.body.appendChild(createWorldContainer());
+  const container = document.getElementById("world");
+  container.appendChild(renderer.domElement);
+
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.update();
+
+  windowHalfX = WIDTH / 2;
+  windowHalfY = HEIGHT / 2;
+
+  document.addEventListener("mousemove", handleMouseMove, false);
+
+  createLights();
+  createFloor();
+  // addGrid();
 }
 
-function render() {
-  if (controls) controls.update();
-  renderer.render(scene, camera);
+function initDog() {
+  const dog = new Dog();
+  addDogToScene(dog.group);
+  loop(dog);
+  tongueToggleController(dog);
+  tongueController(dog);
 }
-
-function component() {
-  const el = document.createElement("div");
-  el.setAttribute("id", "world");
-  return el;
-}
-
-document.body.appendChild(component());
 
 init();
-createLights();
-// addGrid();
-createFloor();
-
-const dog = new Dog();
-
-addDogToScene(dog.group);
-render();
-loop(dog);
-tongueToggleController(dog);
-tongueController(dog);
+initDog();
